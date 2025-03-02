dc288a49a227e6a57ef2a6b97d2ca3106485450e
Justin Rodriguez <jjrod297@gmail.com>
2025-03-01 21:34:41 -0800

    Completed the implementation of option 1. on the menu. It prompts the user what type of student they want to register to the course, along with asking them at the end if they want to add another student of the same type instead of being forced to by the system to add all 50 students of that type at the same time. This allows for a better user experience because it allows them to read, update, or delete student info at the menu and then continue to add another student where they currently left off in line.

--- Program.cs
+++ Program.cs
@@ -2,17 +2,21 @@
 // Course: COMP-003A
 // Faculty: Jonathan Cruz
 // Purpose: Student Course Registration System
+using System.Reflection.Metadata;
+
 namespace COMP003A.FinalProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<Student> student = new List<Student>();
+            List<Student> students = new List<Student>();
+
+            int studentCount = 1;
 
             while (true)
             {
-                Console.WriteLine("Student Course Registration System");
+                Console.WriteLine("\nStudent Course Registration System");
                 
                 Console.WriteLine("\nMain Menu");
                 Console.WriteLine("1. Add Student Information");
@@ -26,7 +30,79 @@ namespace COMP003A.FinalProject
 
                 if (input == 1)
                 {
+                    Console.Write("\nEnter student type (Undergraduate/Graduate): ");
+                    string studentType = Console.ReadLine().ToLower();
 
+                    if (studentType == "undergraduate")
+                    {
+                        for (int i = 0; i < 50; i++)
+                        {
+                             
+                            Console.WriteLine($"\nEnter the details for student {studentCount}");
+
+                            Console.Write("Enter Student ID: ");
+                            int ugstudentID = int.Parse(Console.ReadLine());
+
+                            Console.Write("Enter Student's First Name: ");
+                            string ugfirstName = Console.ReadLine().ToLower();
+
+                            Console.Write("Enter Student's Middle Name (Press Enter to skip): ");
+                            string ugmiddleName = Console.ReadLine().ToLower();
+
+                            Console.Write("Enter Student's Last Name: ");
+                            string uglastName = Console.ReadLine().ToLower();
+
+                            Undergraduate undergraduate = new Undergraduate(ugstudentID, ugfirstName, ugmiddleName, uglastName);
+
+                            students.Add(undergraduate); // added to collection
+
+                            Console.WriteLine($"\nStudent {studentCount} Registration Successful!");
+                            studentCount++;
+
+                            Console.Write("\nAdd another student? (yes/no): ");
+                            string userResponse = Console.ReadLine().ToLower();
+
+                            if (userResponse != "yes")
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else if (studentType == "graduate")
+                    {
+                        for (int i = 0; i < 50; i++)
+                        {
+
+                            Console.WriteLine($"\nEnter the details for student {studentCount}");
+
+                            Console.Write("Enter Student ID: ");
+                            int gstudentID = int.Parse(Console.ReadLine());
+
+                            Console.Write("Enter Student's First Name: ");
+                            string gfirstName = Console.ReadLine().ToLower();
+
+                            Console.Write("Enter Student's Middle Name (Press Enter to skip): ");
+                            string gmiddleName = Console.ReadLine().ToLower();
+
+                            Console.Write("Enter Student's Last Name: ");
+                            string glastName = Console.ReadLine().ToLower();
+
+                            Graduate graduate = new Graduate(gstudentID, gfirstName, gmiddleName, glastName);
+
+                            students.Add(graduate); // added to collection
+
+                            Console.WriteLine($"\nStudent {studentCount} Registration Successful!");
+                            studentCount++;
+
+                            Console.Write("\nAdd another student? (yes/no): ");
+                            string userResponse = Console.ReadLine().ToLower();
+                            
+                            if (userResponse != "yes")
+                            {
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
